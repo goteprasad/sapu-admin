0746f351b7f385840baf2f16540731f204840d31
Prasad Gote <gote.prasad@yahoo.in>
2018-03-05 21:58:27 +0530

    new changes

---
+++ controller/helth.js
@@ -0,0 +1,19 @@
+sapuApp.controller('helthCtrl',function($scope)
+{
+
+$scope.vishal="hsagda";
+   
+    if(localStorage.getItem("user")==null)
+    {
+ 			window.location.href = 'login.html';
+    }
+    else
+    {
+
+
+    }
+    
+   
+
+
+});
